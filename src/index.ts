0a972bed599db22c5eef1d1e6e2746d080df49b5
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-10 09:48:54 -0300

    fix: changed process to performance access

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
 async function handleRequest(event: FetchEvent) {
-    return new Response(JSON.stringify({ time: process.hrtime.bigint() / 1000n; }), {
+    return new Response(JSON.stringify({ time: performance.now() * 1000 }), {
         status: 200,
         headers: new Headers({ "Content-Type": "application/json" }),
     });
