1727725f5c8d3f8df41f8f8c9ec22fb52bb38c83
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-10 21:41:13 -0300

    fix: replaced perf with date

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
 async function handleRequest(event: FetchEvent) {
-    return new Response(JSON.stringify({ time: performance.now() * 1000 }), {
+    return new Response(JSON.stringify({ time: Date.now() * 1000 }), {
         status: 200,
         headers: new Headers({ "Content-Type": "application/json" }),
     });
